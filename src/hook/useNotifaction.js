851c36c247e8b2fbffff9291bf26d337eb3f4341
ziadmustafa1 <124303556+ziadmustafa1@users.noreply.github.com>
2023-08-13 13:46:37 +0300

    ziad

---
+++ src/hook/useNotifaction.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+//to make notifactio to any componentet
+const notify = (msg, type) => {
+    if (type === "warn")
+        toast.warn(msg)
+    else if (type ==="success")
+        toast.success(msg)
+    else if (type ==="error")
+        toast.error(msg)
+};
+
+export default notify;
---
+++ src/hook/user/view-addresses-hook.js
@@ -0,0 +1,25 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllUserAddress } from '../../redux/actions/userAddressesAction';
+
+const ViewAddressesHook = () => {
+    const disptach = useDispatch()
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        const get = async () => {
+            setLoading(true)
+            await disptach(getAllUserAddress());
+            setLoading(false)
+        }
+        get();
+    }, [])
+
+    const res = useSelector(state => state.userAddressesReducer.allAddresses)
+  
+
+    return [res]
+}
+
+export default ViewAddressesHook
